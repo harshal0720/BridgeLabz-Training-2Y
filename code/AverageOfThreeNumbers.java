ad951531b041d019c51aaf8517e5141031b2145a
harshal0720 <harshalgautam2008@gmail.com>
2025-09-02 11:26:15 +0530

    welcome to my codes

---
+++ code/AverageOfThreeNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AverageOfThreeNumbers {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = (a + b + c) / 3;
+    System.out.print(d);
+ }
+}
